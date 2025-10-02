1c457148382d95d8d1c0f228cf20d56902aeb904
Hamza <hamza.sweid963@gmail.com>
2025-10-02 04:14:52 +0400

    change orign url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173', // React dev server
+    origin: 'https://hamza-sweid.github.io/sociat-support',
     methods: ['GET', 'POST'],
     credentials: true,
   })
